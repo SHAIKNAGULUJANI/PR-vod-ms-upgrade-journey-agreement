5d3ab1f64f29ebd4405d7f4850dfd52df254aacd
NAGULUJANISHAIK\windows <Shaik.Nagulujani@vcontractor.co.za>
2021-05-29 13:24:22 +0530

    updated code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# PR-vod-ms-upgrade-journey-agreement
+# vod-ms-upgrade-journey-agreement
+
+
+PLAN DETAILS
+
+localhost:8080/vod-ms-upgrade-journey-agreement/tmf-api/agreementManagement/v4/agreement/27630006826
---
+++ src/controllers/getPlanDetails.js
@@ -0,0 +1,94 @@
+const config = require('config');
+const { Sentry } = require('vod-npm-sentry');
+const sentryCategory = config.get('sentry.categories.getPlanDetails');
+const contractService = require('vod-npm-services/vod-ms-account-summary');
+const prometheusClient = require('restify-prom-bundle').client;
+const getPlanDetailError = new prometheusClient.Counter({
+  name: 'counter_get_plan_detail_error',
+  help: 'vod-ms-account-summary getPlanDetails map call error'
+});
+
+exports.handler = async function getPlanDetails(req, res, next) {
+  Sentry.info('Beginning getPlanDetails...', {}, sentryCategory);
+  const params = {
+    headers: req.headers,
+    msisdn: req.params.msisdn
+  };
+  const response = await contractService.getPlanDetails(req, params);
+  let output = '';
+  if (response.ok === false && response.error.status === 400) {
+    output = {
+      code: response.error.status,
+      status: response.error.name,
+      message: 'Invalid Msisdn'
+    };
+    getPlanDetailError.inc();
+    res.status(output.code);
+    res.json(output);
+    return next(response.error);
+}
+  else if (response.ok === false) {
+    output = {
+      code: response.error.response.status,
+      status: response.error.response.statusText,
+      message: response.error.response.data.result.exceptionMessage
+    };
+    getPlanDetailError.inc();
+    res.status(output.code);
+    res.json(output);
+    return next(response.error);
+  }
+  else {
+    const planList = {
+      agreementItem: [
+        {
+          product: [
+            {
+              id: response.data.result.planCode,
+              name: response.data.result.plan
+            }
+          ],
+          termOrCondition: [
+            {
+              id: response.data.result.planCode,
+              validFor: {
+                endDateTime: response.data.result.endDate,
+                startDateTime: response.data.result.startDate
+              }
+            }
+          ]
+        }
+      ],
+      characteristic: [
+        {
+          name: 'upgradeDue',
+          value: response.data.result.upgradeDue
+        },
+        {
+          name: 'redirectLink',
+          value: response.data.result.banner.redirectLink
+        },
+        {
+          name: 'imageLink',
+          value: response.data.result.banner.imageLink
+        },
+        {
+          name: 'processing',
+          value: response.data.result.processing
+        },
+        {
+          name: 'successful',
+          value: response.data.successful
+        },
+        {
+          name: 'code',
+          value: response.data.code
+        }
+      ]
+    };
+
+    res.status(response.status);
+    res.json(planList);
+    return next();
+  }
+};
